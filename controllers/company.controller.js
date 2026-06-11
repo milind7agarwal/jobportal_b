389d11662d81eb9c8e0284d8557c41135be6da38
Milind Agarwal <agarwalmilind77@gmail.com>
2026-06-11 21:59:05 +0530

    added multer properties to company

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -1,4 +1,6 @@
 import Company from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from '../utils/cloud.js';
 
 export const registerCompany = async(req, res) => {
     try {
@@ -59,7 +61,11 @@ export const updateCompany = async (req, res) => {
     const { name, description, website, location } = req.body;
     const file = req.file;
     // cloudinary
-    const updateData = { name, description, website, location};
+    const fileUri = getDataUri(file);
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+
+    const logo = cloudResponse.secure_url;
+    const updateData = { name, description, website, location,logo};
 
     const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
       new: true,
